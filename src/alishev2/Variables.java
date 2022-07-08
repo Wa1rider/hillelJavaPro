debd900e360e3b7788d77eb53db782019f1d664d
Wa1rider <maxmakcmax@gmail.com>
2022-07-08 17:07:39 +0300

    Alishev video 1-3 done

---
+++ src/alishev2/Variables.java
@@ -0,0 +1,18 @@
+package alishev2;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short myShort = 3266;
+        long myLong = 26246246;
+
+        double myDouble = 235.35;
+        float nyFloat = 2362.4f;
+
+        char c = 'c';
+        boolean b = true;
+
+        byte myByte = 100; // -128 - 127
+        System.out.println(myInt);
+    }
+}
